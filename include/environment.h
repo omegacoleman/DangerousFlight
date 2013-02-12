a87bd94b29d8bc8540784247f8e76c9aede47ca7
Stephen Coleman <omegacoleman@gmail.com>
2013-02-12 20:11:30 +0800

    Add environment.h.

---
+++ include/environment.h
@@ -0,0 +1,14 @@
+
+#ifndef _ENVIRONMENT_H_
+#define _ENVIRONMENT_H_
+
+typedef struct
+{
+    SDL_Surface *bg;
+    SDL_Surface *speclines;
+    SDL_Surface *foglines;
+} SuperBlitable;
+
+#endif
+
+
